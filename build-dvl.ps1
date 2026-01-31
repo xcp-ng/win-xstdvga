5a648889f6206df17e380346b525093d10d9c6ed
Tu Dinh <ngoc-tu.dinh@vates.tech>
2026-01-31 20:10:17 +0100

    add build-dvl workflow
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- build-dvl.ps1
+++ build-dvl.ps1
@@ -22,10 +22,19 @@ try {
     Remove-Item -Force -Recurse $SolutionDir\$Platform\$Configuration -ErrorAction SilentlyContinue
     Remove-Item -Force -Recurse database -ErrorAction SilentlyContinue
     codeql database create database --language=cpp --source-root=. --command="powershell.exe -file .\build.ps1 -Configuration $Configuration -Platform $Platform -Project $Project -SolutionDir $SolutionDir -CodeAnalysis"
+    if ($LASTEXITCODE -ne 0) {
+        throw "CodeQL failed with error $LASTEXITCODE"
+    }
     codeql database analyze database microsoft/windows-drivers@1.8.0:windows-driver-suites/recommended.qls --format=sarifv2.1.0 --output=$SolutionDir\$Platform\$Configuration\$Project.sarif
+    if ($LASTEXITCODE -ne 0) {
+        throw "CodeQL failed with error $LASTEXITCODE"
+    }
     Push-Location $SolutionDir\$Platform\$Configuration
     try {
         & "C:\Program Files (x86)\Windows Kits\10\Tools\dvl\dvl.exe" /manualCreate $Project $Platform /sarifPath .
+        if ($LASTEXITCODE -ne 0) {
+            throw "dvl.exe failed with error $LASTEXITCODE"
+        }
     }
     finally {
         Pop-Location
