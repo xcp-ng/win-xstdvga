9ee2b9b879e7a4276d942bbd77b9d8e6b4540bac
Tu Dinh <ngoc-tu.dinh@vates.tech>
2026-02-01 23:23:48 +0100

    add codeql to actions runner
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- build-dvl.ps1
+++ build-dvl.ps1
@@ -12,7 +12,11 @@ param (
     [string]$Project = "xstdvga",
     [Parameter()]
     [ValidateSet("vs2022")]
-    [string]$SolutionDir = "vs2022"
+    [string]$SolutionDir = "vs2022",
+    [Parameter()]
+    [string]$CodeQL = "codeql",
+    [Parameter()]
+    [string]$DVL = "C:\Program Files (x86)\Windows Kits\10\Tools\dvl\dvl.exe"
 )
 
 $ErrorActionPreference = "Stop"
@@ -21,17 +25,17 @@ Push-Location $PSScriptRoot
 try {
     Remove-Item -Force -Recurse $SolutionDir\$Platform\$Configuration -ErrorAction SilentlyContinue
     Remove-Item -Force -Recurse database -ErrorAction SilentlyContinue
-    codeql database create database --language=cpp --source-root=. --command="powershell.exe -file .\build.ps1 -Configuration $Configuration -Platform $Platform -Project $Project -SolutionDir $SolutionDir -CodeAnalysis"
+    & $CodeQL database create database --language=cpp --source-root=. --command="powershell.exe -file .\build.ps1 -Configuration $Configuration -Platform $Platform -Project $Project -SolutionDir $SolutionDir -CodeAnalysis"
     if ($LASTEXITCODE -ne 0) {
         throw "CodeQL failed with error $LASTEXITCODE"
     }
-    codeql database analyze database microsoft/windows-drivers@1.8.0:windows-driver-suites/recommended.qls --format=sarifv2.1.0 --output=$SolutionDir\$Platform\$Configuration\$Project.sarif
+    & $CodeQL database analyze database microsoft/windows-drivers@1.8.2:windows-driver-suites/recommended.qls --format=sarifv2.1.0 --output=$SolutionDir\$Platform\$Configuration\$Project.sarif
     if ($LASTEXITCODE -ne 0) {
         throw "CodeQL failed with error $LASTEXITCODE"
     }
     Push-Location $SolutionDir\$Platform\$Configuration
     try {
-        & "C:\Program Files (x86)\Windows Kits\10\Tools\dvl\dvl.exe" /manualCreate $Project $Platform /sarifPath .
+        & $DVL /manualCreate $Project $Platform /sarifPath .
         if ($LASTEXITCODE -ne 0) {
             throw "dvl.exe failed with error $LASTEXITCODE"
         }
